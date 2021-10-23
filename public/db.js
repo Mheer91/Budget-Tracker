b0a02f55bdd29e2f17df4b7ac3e2563e3e224e03
Michael Heer <mheer91@icloud.com>
2021-10-22 22:42:35 -0500

    Offline functionality w/ db sync

--- public/db.js
+++ public/db.js
@@ -1,5 +1,61 @@
-function test () {
-    console.log("You are now online!")
-}
+const request = indexedDB.open("budget", 1);
+let db;
 
-window.addEventListener("online", test);
+request.onupgradeneeded = ({ target }) => {
+    const db = target.result;
+    db.createObjectStore(('transaction'), {autoIncrement: true});
+};
+
+request.onsuccess = ({ target }) => {
+     db = target.result;
+
+    if (navigator.onLine) { 
+        console.log("You are currently online!")
+        dbSync();
+    }
+};
+
+request.onerror = (err) => {
+    console.log(err.message)
+};
+
+
+function saveRecord (record) {
+    const transaction = db.transaction(['transaction'], 'readwrite');
+    const budgetStore = transaction.objectStore('transaction');
+
+    budgetStore.add(record);
+};
+
+function dbSync () {
+    console.log("Databases syncing")
+    const transaction = db.transaction(['transaction'], 'readonly');
+    const budgetStore = transaction.objectStore('transaction');
+    const pendingTransactions = budgetStore.getAll();
+
+    pendingTransactions.onsuccess = () => {
+        if (pendingTransactions.result.length > 0) {
+            console.log("Pending transactions being added")
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(pendingTransactions.result),
+                headers: {
+                  Accept: "application/json, text/plain, */*",
+                  "Content-Type": "application/json"
+                }
+              })
+              .then(response => response.json())
+              .then(() => {
+                  const transaction = db.transaction(['transaction'], 'readwrite');
+                  const budgetStore = transaction.objectStore('transaction');
+                  budgetStore.clear();
+                  console.log("Database sync complete. All transactions have been added!")
+              })
+              .catch((err) => {
+                  console.log(err)
+              });
+        };
+    };
+};
+
+window.addEventListener("online", dbSync);
